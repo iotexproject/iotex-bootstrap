81c06fc21efce2c4ba0d4c310036a2cf9ea2e86b
feiren.kuang <feigu_1984@qq.com>
2019-10-31 16:01:42 +0800

    add change externalHost,producerPrivKey config.yml
    1,  install:  add input  externalHost, producerPrivKey config item,
    2, Upgrade:  get user old config ( externalHost, producerPrivKey), insert  new config.yaml

--- scripts/setup_fullnode.sh
+++ scripts/setup_fullnode.sh
@@ -3,46 +3,72 @@
 ##Setup & Upgrade Iotex MainNet
 ##User bash/sh $0
 
+##Input Version
 lastversion=$(curl -sS https://raw.githubusercontent.com/iotexproject/iotex-bootstrap/master/README.md|grep "^- MainNet:"|awk '{print$3}')
 defaultdatadir="$HOME/iotex-var"
-
-read -p "Install or Upgrade Version default $lastversion: " ver
+read -p "Install or Upgrade Version [$lastversion]: " ver
 version=${ver:-"$lastversion"}   # if $ver ;then version=$ver;else version=$lastversion"
+
+##Input Data Dir
+echo "The current user of the installation directory must have write permission!!!"
+read -p "Install OR Upgrade Input your data dir [$defaultdatadir]: " inputdir
+datadir=${inputdir:-"$defaultdatadir"}
+
 echo "Confirm version: ${version}"
+echo "Confirm Data directory: ${datadir}"
+read -p "Press any key to continue ... [Ctrl + c exit!] " key1
+export IOTEX_HOME=$datadir
 
 ##check iotex-server exist and running
-currversion=$(docker ps -a |grep "iotex/iotex-core:v"|awk '{print$2}'|awk -F'[:]' '{print$2}')
+runversion=$(docker ps -a |grep "iotex/iotex-core:v"|awk '{print$2}'|awk -F'[:]' '{print$2}')
 
-if [ ${currversion} == $version ];then
-    echo "Not Upgrade"
-    exit 0
-elif [ ${currversion} ]; then
-    echo "Stop old iotex-core"
-    docker stop iotex
-    echo "delete docker container"
-    docker rm iotex
-    echo "delete iotex images"
-    docker rmi $(docker images iotex/iotex-core -q)
+if [ ${runversion} ];then
+
+    if [ $version = ${runversion} ];then
+        echo "Not Upgrade!! current ${runversion} is latest version"
+        exit 0
+    else
+        echo "Stop old iotex-core"
+        docker stop iotex
+        echo "delete docker container"
+        docker rm iotex
+        #echo "delete iotex images"
+        #docker rmi $(docker images iotex/iotex-core -q)
+        producerPrivKey=$(grep '^  producerPrivKey:' ${datadir}/etc/config.yaml|sed 's/^  //g')
+        externalHost=$(grep '^  externalHost:' ${datadir}/etc/config.yaml|sed 's/^  //g')
+    fi
+
+else
+    mkdir -p ${datadir} && cd ${datadir} && mkdir data log etc
+    findip=$(curl -Ss ip.sb)
+    read -p "SET YOUR EXTERNAL IP HERE [$findip]: " inputip
+    echo "If you are a delegate, make sure producerPrivKey is the key for the operator address you have registered."
+    echo  "SET YOUR PRIVATE KEY HERE(e.g., 96f0aa5e8523d6a28dc35c927274be4e931e74eaa720b418735debfcbfe712b8)"
+    read -p ": " inputkey
+    ip=${inputip:-$findip}
+    PrivKey=${inputkey:-"96f0aa5e8523d6a28dc35c927274be4e931e74eaa720b418735debfcbfe712b8"}
+    echo "Confirm your externalHost: $ip"
+    echo "Confirm your producerPrivKey: $PrivKey"
+    read -p "Press any key to continue ... [Ctrl + c exit!] " key2
+    externalHost="externalHost: $ip"
+    producerPrivKey="producerPrivKey: $PrivKey"
 fi
-echo "The current user of the installation directory must have write permission!!!"
-read -p "Install Data directory or Upgrade old Data directory default $defaultdatadir: " inputdir
-datadir=${inputdir:-"$defaultdatadir"}
-echo "Confirm Data directory: ${datadir}"
-mkdir -p ${datadir} && cd ${datadir} && mkdir data log etc
 
 docker pull iotex/iotex-core:${version}
-
 # or use gcr.io/iotex-servers/iotex-core:${version}
-
 #Set the environment with the following commands:
 
-export IOTEX_HOME=$datadir
 #(Optional) If you prefer to start from a snapshot, run the following commands:
 #curl -LSs https://t.iotex.me/mainnet-data-latest > $IOTEX_HOME/data.tar.gz
 #cd ${IOTEX_HOME} && tar -xzf data.tar.gz
 curl -Ss https://raw.githubusercontent.com/iotexproject/iotex-bootstrap/master/config_mainnet.yaml > $IOTEX_HOME/etc/config.yaml
 curl -Ss https://raw.githubusercontent.com/iotexproject/iotex-bootstrap/master/genesis_mainnet.yaml > $IOTEX_HOME/etc/genesis.yaml
-# sed -i '/^chain:/a\  producerPrivKey: d02921f3bf63c4dff0673b0446b552174deea243c5a1d32717d51b1807bcef76' $IOTEX_HOME/etc/config.yaml
+
+
+echo "Update your externalHost,producerPrivKey to config.yaml"
+sed -i "/^network:/a\ \ $externalHost" $IOTEX_HOME/etc/config.yaml
+sed -i "/^chain:/a\ \ $producerPrivKey" $IOTEX_HOME/etc/config.yaml
+
 #Run the following command to start a node:
 docker run -d --restart on-failure --name iotex \
         -p 4689:4689 \
@@ -57,5 +83,5 @@ docker run -d --restart on-failure --name iotex \
         -genesis-path=/etc/iotex/genesis.yaml
 
 #check node running
-docker ps | grep iotex-server
 
+docker ps | grep iotex-server
