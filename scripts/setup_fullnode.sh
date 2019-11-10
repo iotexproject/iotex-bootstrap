d462d4e8b5e0663358dcda50655b39418c5f1df0
feiren.kuang <feigu_1984@qq.com>
2019-11-10 08:02:39 +0800

    remind update directory
    Judge the latest version and remind update directory

--- scripts/setup_fullnode.sh
+++ scripts/setup_fullnode.sh
@@ -21,39 +21,51 @@ else
 fi
 defaultdatadir="$HOME/iotex-var"
 echo -e "Current operating environment: ${YELLOW}  $env ${NC}"
-read -p "Install or Upgrade Version, if null the latest [$lastversion]: " ver
+#while True; do
+read -p "Install or Upgrade Version; if null the latest [$lastversion]: " ver
+
 version=${ver:-"$lastversion"}   # if $ver ;then version=$ver;else version=$lastversion"
 
-##Input Data Dir
-echo "The current user of the installation directory must have write permission!!!"
-read -p "Install OR Upgrade your \$IOTEX_HOME [e.g., $defaultdatadir]: " inputdir
-IOTEX_HOME=${inputdir:-"$defaultdatadir"}
-
-echo -e "Confirm version: ${RED} ${version} ${NC}"
-echo -e "Confirm Data directory: ${RED} ${IOTEX_HOME} ${NC}"
-read -p "Press any key to continue ... [Ctrl + c exit!] " key1
-
-
-##check iotex-server exist and running
 runversion=$(docker ps -a |grep "iotex/iotex-core:v"|awk '{print$2}'|awk -F'[:]' '{print$2}')
 
-if [ ${runversion} ];then
+if [ "$version"X = "$runversion"X ];then
+    echo "Not Upgrade!! current ${runversion} is running....!"
+    exit 0
+fi
+##Input Data Dir
+echo "The current user of the input directory must have write permission!!!"
+echo -e "${RED} If Upgrade ; input your old directory \$IOTEX_HOME !!! ${NC}"
 
-    if [ "$version"X = "$runversion"X ];then
-        echo "Not Upgrade!! current ${runversion} is latest version"
-        exit 0
-    else
-        echo "Stop old iotex-core"
-        docker stop iotex
-        echo "delete old iotex docker container"
-        docker rm iotex
+#while True: do
+read -p "Input your \$IOTEX_HOME [e.g., $defaultdatadir]: " inputdir
+IOTEX_HOME=${inputdir:-"$defaultdatadir"}
+
+#if [ "$IOTEX_HOME" ];
+
+echo -e "Confirm version: ${RED} ${version} ${NC}"
+echo -e "Confirm IOTEX_HOME directory: ${RED} ${IOTEX_HOME} ${NC}"
+read -p "Press any key to continue ... [Ctrl + c exit!] " key1
+
+if [ -f "${IOTEX_HOME}/data/chain.db" ]; then
+    echo -e "${YELLOW} ******  Upgrade Iotex Node ******* ${NC}"
+    echo -e "${YELLOW} ***  Will stop, delete old iotex container; ${NC}"
+    echo -e "${YELLOW} *** download new config and recover your externalHost producerPrivKey ${NC}"
+    read -p "******* Press any key to continue ... [Ctrl + c exit!] " upgreadekey
+    echo "Stop old iotex-core"
+    docker stop iotex
+    echo "delete old iotex docker container"
+    docker rm iotex
         #echo "delete iotex images"
         #docker rmi $(docker images iotex/iotex-core -q)
-        producerPrivKey=$(grep '^  producerPrivKey:' ${IOTEX_HOME}/etc/config.yaml|sed 's/^  //g')
-        externalHost=$(grep '^  externalHost:' ${IOTEX_HOME}/etc/config.yaml|sed 's/^  //g')
-    fi
+    producerPrivKey=$(grep '^  producerPrivKey:' ${IOTEX_HOME}/etc/config.yaml|sed 's/^  //g')
+    externalHost=$(grep '^  externalHost:' ${IOTEX_HOME}/etc/config.yaml|sed 's/^  //g')
 
 else
+    echo -e "${YELLOW} ****** Install Iotex Node  ***** ${NC}"
+    echo -e "${YELLOW} if installed, Confirm Input IOTEX_HOME directory True ${NC};"
+    read -p "[Ctrl + c exit!]; else Enter anykey ..." anykey
+
+
     mkdir -p ${IOTEX_HOME} && cd ${IOTEX_HOME} && mkdir data log etc
     findip=$(curl -Ss ip.sb)
     read -p "SET YOUR EXTERNAL IP HERE [$findip]: " inputip
@@ -69,6 +81,8 @@ else
     producerPrivKey="producerPrivKey: $PrivKey"
 fi
 
+
+echo "docker pull iotex-core ${version}"
 docker pull iotex/iotex-core:${version}
 # or use gcr.io/iotex-servers/iotex-core:${version}
 #Set the environment with the following commands:
@@ -76,6 +90,7 @@ docker pull iotex/iotex-core:${version}
 #(Optional) If you prefer to start from a snapshot, run the following commands:
 #curl -LSs https://t.iotex.me/${env}-data-latest > $IOTEX_HOME/data.tar.gz
 #cd ${IOTEX_HOME} && tar -xzf data.tar.gz
+echo "download new config"
 curl -Ss https://raw.githubusercontent.com/iotexproject/iotex-bootstrap/${version}/config_${env}.yaml > $IOTEX_HOME/etc/config.yaml
 curl -Ss https://raw.githubusercontent.com/iotexproject/iotex-bootstrap/${version}/genesis_${env}.yaml > $IOTEX_HOME/etc/genesis.yaml
 # https://raw.githubusercontent.com/iotexproject/iotex-bootstrap/v0.9.2/config_mainnet.yaml
@@ -84,6 +99,7 @@ echo "Update your externalHost,producerPrivKey to config.yaml"
 sed -i "/^network:/a\ \ $externalHost" $IOTEX_HOME/etc/config.yaml
 sed -i "/^chain:/a\ \ $producerPrivKey" $IOTEX_HOME/etc/config.yaml
 
+echo -e "docker run iotex: ${YELLOW} ${version} ${NC}"
 #Run the following command to start a node:
 docker run -d --restart on-failure --name iotex \
         -p 4689:4689 \
@@ -98,5 +114,5 @@ docker run -d --restart on-failure --name iotex \
         -genesis-path=/etc/iotex/genesis.yaml
 
 #check node running
-
+sleep 5
 docker ps | grep iotex-server
